d7e20f412cc628cecbfd95cef9c84ad8bd15eb82
Kathir-Subramaniam <kathir.arunjunai@code.berlin>
2025-11-19 17:48:56 +0100

    fix:Cleaned up for deployment

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const firebase = require("firebase/app");
+const admin = require("firebase-admin");
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -7,24 +8,22 @@ const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword, 
-  signOut, 
-  sendEmailVerification, 
-  sendPasswordResetEmail
+const {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+} = require("firebase/auth");
 
-} = require("firebase/auth") ;
-
-const admin = require('firebase-admin');
-const serviceAccount = require("../firebaseService.json");
-
+const serviceJson = process.env.FIREBASE_SERVICE_ACCOUNT;
+const serviceAccount = JSON.parse(serviceJson);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -36,5 +35,46 @@ module.exports = {
   signOut,
   sendEmailVerification,
   sendPasswordResetEmail,
-  admin
+  admin,
 };
+
+// require("dotenv").config();
+// const firebase = require("firebase/app");
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// const { 
+//   getAuth, 
+//   createUserWithEmailAndPassword, 
+//   signInWithEmailAndPassword, 
+//   signOut, 
+//   sendEmailVerification, 
+//   sendPasswordResetEmail
+
+// } = require("firebase/auth") ;
+
+// const admin = require('firebase-admin');
+// const serviceAccount = require("../firebaseService.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// module.exports = {
+//   getAuth,
+//   signInWithEmailAndPassword,
+//   createUserWithEmailAndPassword,
+//   signOut,
+//   sendEmailVerification,
+//   sendPasswordResetEmail,
+//   admin
+// };
